495051b6a7b30150b6435a324548f5390d3ecd1e
xqdd <1032453509@qq.com>
2025-01-11 15:41:04 +0800

    提供一些默认配色

--- popup/popup.js
+++ popup/popup.js
@@ -58,20 +58,20 @@ function initStyle() {
         , "highlightText"
         , "bubbleBackground"
         , "bubbleText"], function (result) {
-        highlightBackground.val(result.highlightBackground)
-        highlightText.val(result.highlightText)
-        highlightTemplate.css("background-color", result.highlightBackground)
-        if (result.highlightText.trim() !== "") {
-            highlightTemplate.css("color", result.highlightText)
-        }
-        bubbleBackground.val(result.bubbleBackground)
-        bubbleText.val(result.bubbleText)
-        bubbleTemplate.css("background-color", result.bubbleBackground)
-        if (result.bubbleText.trim() !== "") {
-            bubbleTemplate.css("color", result.bubbleText)
-        }
-        chrome.runtime.sendMessage({type: "setStyle"})
-    })
+            highlightBackground.val(result.highlightBackground)
+            highlightText.val(result.highlightText)
+            highlightTemplate.css("background-color", result.highlightBackground)
+            if (result.highlightText.trim() !== "") {
+                highlightTemplate.css("color", result.highlightText)
+            }
+            bubbleBackground.val(result.bubbleBackground)
+            bubbleText.val(result.bubbleText)
+            bubbleTemplate.css("background-color", result.bubbleBackground)
+            if (result.bubbleText.trim() !== "") {
+                bubbleTemplate.css("color", result.bubbleText)
+            }
+            chrome.runtime.sendMessage({ type: "setStyle" })
+        })
 }
 
 
@@ -184,7 +184,7 @@ xmlFile.on("change", function () {
                         phonetic: node.find("phonetic").text()
                     }
                 })
-                chrome.storage.local.set({newWords: {wordInfos}})
+                chrome.storage.local.set({ newWords: { wordInfos } })
                 //提示导入成功
                 // chrome.notifications.create({
                 //     type: "basic",
@@ -211,61 +211,61 @@ ttsVoices.on("click", function () {
     let val = $("#tts_voices option:selected").val();
     if (val && val !== "" && val !== "无") {
         let ttsVoices = JSON.parse(val);
-        chrome.storage.local.set({ttsVoices})
+        chrome.storage.local.set({ ttsVoices })
     }
 })
 ttsToggle.on("click", function () {
-    chrome.storage.local.set({ttsToggle: ttsToggle.prop("checked")})
+    chrome.storage.local.set({ ttsToggle: ttsToggle.prop("checked") })
 })
 toggle.on("click", function () {
-    chrome.storage.local.set({toggle: toggle.prop("checked")})
+    chrome.storage.local.set({ toggle: toggle.prop("checked") })
 })
 autoSync.on("click", function () {
-    chrome.storage.local.set({autoSync: autoSync.prop("checked")})
+    chrome.storage.local.set({ autoSync: autoSync.prop("checked") })
 })
 highlightText.on("input", function () {
-    chrome.storage.local.set({highlightText: highlightText.val()}, function () {
+    chrome.storage.local.set({ highlightText: highlightText.val() }, function () {
         initStyle()
     })
 })
 highlightBackground.on("input", function () {
-    chrome.storage.local.set({highlightBackground: highlightBackground.val()}, function () {
+    chrome.storage.local.set({ highlightBackground: highlightBackground.val() }, function () {
         initStyle()
     })
 })
 bubbleText.on("input", function () {
-    chrome.storage.local.set({bubbleText: bubbleText.val()}, function () {
+    chrome.storage.local.set({ bubbleText: bubbleText.val() }, function () {
         initStyle()
     })
 })
 bubbleBackground.on("input", function () {
-    chrome.storage.local.set({bubbleBackground: bubbleBackground.val()}, function () {
+    chrome.storage.local.set({ bubbleBackground: bubbleBackground.val() }, function () {
         initStyle()
     })
 })
 reset.on("click", function () {
     if (window.confirm("确认恢复默认设置？")) {
-        chrome.runtime.sendMessage({type: "resetSettings"}, function () {
+        chrome.runtime.sendMessage({ type: "resetSettings" }, function () {
             initSettings()
         })
     }
 })
 $("#help").on("click", function () {
-    chrome.tabs.create({url: 'https://github.com/XQDD/highlight_new_words'});
+    chrome.tabs.create({ url: 'https://github.com/XQDD/highlight_new_words' });
 })
 $("#moreVoices").on("click", function () {
-    chrome.tabs.create({url: 'https://chrome.google.com/webstore/detail/speakit/pgeolalilifpodheeocdmbhehgnkkbak'});
+    chrome.tabs.create({ url: 'https://chrome.google.com/webstore/detail/speakit/pgeolalilifpodheeocdmbhehgnkkbak' });
 })
 youdaoLoginATag.on("click", function () {
-    chrome.tabs.create({url: 'http://dict.youdao.com/wordbook/wordlist'});
+    chrome.tabs.create({ url: 'http://dict.youdao.com/wordbook/wordlist' });
 })
 oluLoginATag.on("click", function () {
-    chrome.tabs.create({url: 'https://my.eudic.net/studylist'});
+    chrome.tabs.create({ url: 'https://my.eudic.net/studylist' });
 })
 syncButton.click(function () {
     let clickedSyncButton = $(this)
     clickedSyncButton.attr("disabled", "")
-    chrome.runtime.sendMessage({type: "sync", dictionaryType: $(this).attr("dictionaryType")}, function (msg) {
+    chrome.runtime.sendMessage({ type: "sync", dictionaryType: $(this).attr("dictionaryType") }, function (msg) {
         if (msg) {
             alert(msg)
         }
@@ -273,6 +273,23 @@ syncButton.click(function () {
         clickedSyncButton.removeAttr("disabled")
     })
 })
+
+$(".color-scheme-button").on("click", function () {
+    const bgColor = $(this).data("bg");
+    const textColor = $(this).data("text");
+    highlightBackground.val(bgColor);
+    highlightText.val(textColor);
+    bubbleBackground.val(bgColor);
+    bubbleText.val(textColor);
+    chrome.storage.local.set({
+        highlightBackground: bgColor,
+        highlightText: textColor,
+        bubbleBackground: bgColor,
+        bubbleText: textColor
+    }, function () {
+        initStyle();
+    });
+});
 
 
 initSettings()
