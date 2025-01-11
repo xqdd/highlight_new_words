b4bcef4b180214ee1f045cf382c43772043a1def
xqdd <1032453509@qq.com>
2025-01-11 14:23:44 +0800

    feat: 兼容v3版本

--- service_worker.js
+++ service_worker.js
@@ -27,23 +27,37 @@ function setStyle(result) {
     }
     highlightCss += "}";
     bubbleCss += "}";
-    chrome.tabs.insertCSS(null, {code: highlightCss + bubbleCss});
+
+    // 获取所有标签页
+    chrome.tabs.query({}, function (tabs) {
+        tabs.forEach(function (tab) {
+            // 为每个标签页插入样式
+            chrome.scripting.insertCSS({
+                target: { tabId: tab.id },
+                css: highlightCss + bubbleCss
+            });
+        });
+    });
 }
 
 //初始化配置
-chrome.storage.local.get(["ttsVoices", "syncTime", "autoSync", "cookie", "dictionaryType"], function (result) {
+chrome.storage.local.get(["ttsVoices", "syncTime", "autoSync", "cookie", "dictionaryType"], function (result, b, c) {
+    console.log(result)
+    console.log(b)
+    console.log(c)
     if (!result.ttsVoices) {
         initSettings();
     }
     if (result.autoSync && (new Date().getTime() - result.syncTime) > 60 * 60 * 24 * 1000) {
         console.log("同步中。。。")
-        sync(() => {
+        sync((msg) => {
+            console.log(msg)
         }, result.dictionaryType)
     }
 });
 
 function syncSuccess(wordInfos, cookie, sendResponse, dictionaryType) {
-    chrome.storage.local.set({newWords: {wordInfos}, dictionaryType, cookie, syncTime: new Date().getTime()}, () => {
+    chrome.storage.local.set({ newWords: { wordInfos }, dictionaryType, cookie, syncTime: new Date().getTime() }, () => {
         sendResponse("同步成功");
     });
 }
@@ -52,41 +66,47 @@ let sync = function (sendResponse, dictionaryType) {
     //同步有道词典
     if (dictionaryType == 0) {
         getCookie(cookie => {
-            axios.get("http://dict.youdao.com/wordbook/webapi/words?limit=100000000&offset=0").then(({data: {data}}) => {
-                let wordInfos = {};
-                if (data.total === 0) {
-                    sendResponse("同步成功，但生词本无内容，若实际有内容，请尝试重新登录");
-                    return
-                }
-                data.itemList.forEach(w => {
-                    wordInfos[w.word.toLowerCase()] = w
+            fetch("http://dict.youdao.com/wordbook/webapi/words?limit=100000000&offset=0")
+                .then(res => res.json())
+                .then(({ data }) => {
+                    console.log(data)
+
+                    let wordInfos = {};
+                    if (data.total === 0) {
+                        sendResponse("同步成功，但生词本无内容，若实际有内容，请尝试重新登录");
+                        return
+                    }
+                    data.itemList.forEach(w => {
+                        wordInfos[w.word.toLowerCase()] = w
+                    });
+                    syncSuccess(wordInfos, cookie, sendResponse, dictionaryType);
+                }).catch(e => {
+                    sendResponse("网络错误或服务器出错，请检查网络后再试或重新登录");
                 });
-                syncSuccess(wordInfos, cookie, sendResponse, dictionaryType);
-            }).catch(e => {
-                sendResponse("网络错误或服务器出错，请检查网络后再试或重新登录");
-            });
         })
     }
     //同步欧路词典
     else if (dictionaryType == 1) {
-        axios.get("https://my.eudic.net/StudyList/WordsDataSource?start=0&length=100000000").then(({data}) => {
-            if (data.data) {
-                let wordInfos = {};
-                data.data.forEach(item => {
-                    wordInfos[item.uuid.toLowerCase()] = {
-                        phonetic: item.phon,
-                        trans: item.exp,
-                        word: item.uuid,
-                        link: item.word,
-                    }
-                });
-                syncSuccess(wordInfos, null, sendResponse, dictionaryType);
-            } else {
-                sendResponse("同步失败，请尝试重新登录");
-            }
-        }).catch(e => {
-            sendResponse("网络错误或服务器出错，请检查网络后再试或重新登录");
-        });
+        fetch("https://my.eudic.net/StudyList/WordsDataSource?start=0&length=100000000")
+            .then(res => res.json())
+            .then(({ data }) => {
+                if (data) {
+                    let wordInfos = {};
+                    data.forEach(item => {
+                        wordInfos[item.uuid.toLowerCase()] = {
+                            phonetic: item.phon,
+                            trans: item.exp,
+                            word: item.uuid,
+                            link: item.word,
+                        }
+                    });
+                    syncSuccess(wordInfos, null, sendResponse, dictionaryType);
+                } else {
+                    sendResponse("同步失败，请尝试重新登录");
+                }
+            }).catch(e => {
+                sendResponse("网络错误或服务器出错，请检查网络后再试或重新登录");
+            });
     }
 };
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
@@ -95,7 +115,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         if (!!request.word && typeof request.word === "string") {
             chrome.storage.local.get(["ttsToggle", "ttsVoices"], function (result) {
                 if (result.ttsToggle) {
-                    chrome.tts.speak(request.word, {...result.ttsVoices});
+                    chrome.tts.speak(request.word, { ...result.ttsVoices });
                 }
             })
         }
@@ -114,15 +134,21 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         let word = wordData.word;
         chrome.storage.local.get(["dictionaryType"], function (result) {
             if (result.dictionaryType == 0) {
-                axios.get(`http://dict.youdao.com/wordbook/webapi/delete?itemId=${wordData.itemId}`)
+                fetch(`http://dict.youdao.com/wordbook/webapi/delete?itemId=${wordData.itemId}`)
             } else if (result.dictionaryType == 1) {
-                axios.post("https://dict.eudic.net/Dicts/SetStarRating", {rating: -1, word, lang: 'en'})
+                fetch("https://dict.eudic.net/Dicts/SetStarRating", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ rating: -1, word, lang: 'en' })
+                })
             }
         })
         chrome.storage.local.get("newWords", function (result) {
             let wordInfos = result.newWords.wordInfos;
             delete wordInfos[word.toLowerCase()];
-            chrome.storage.local.set({newWords: {wordInfos}});
+            chrome.storage.local.set({ newWords: { wordInfos } });
             sendResponse("删除成功，刷新页面后生效");
         });
     } else if (request.type === "count") {
@@ -149,7 +175,7 @@ function getCookie(callback) {
         if (!!result.cookie) {
             callback(result.cookie)
         } else {
-            chrome.cookies.getAll({url: "http://dict.youdao.com/wordbook/wordlist"}, cookies => {
+            chrome.cookies.getAll({ url: "http://dict.youdao.com/wordbook/wordlist" }, cookies => {
                 cookies.map(c => c.name + "=" + c.value).join(";")
                 callback(cookies.map(c => c.name + "=" + c.value).join(";"))
             })
@@ -179,16 +205,19 @@ chrome.tabs.onUpdated.addListener(function (id, info, tab) {
 
 function updateWordCount(tabId) {
     if (wordCount[tabId]) {
-        chrome.browserAction.getBadgeText({tabId}, function (countStr) {
-            const count = wordCount[tabId].size
-            if ((!countStr || countStr === "") && count === 0) {
-                return
+        const count = wordCount[tabId].size;
+
+        chrome.action.getBadgeText({ tabId }, function (countStr) {
+            if (count === 0 && (!countStr || countStr === "")) {
+                // 如果当前计数为0且徽章文字为空，则什么都不做
+                return;
             }
-            chrome.browserAction.setBadgeText({
-                text: count + "",
+            // 将徽章文字设置为当前的单词计数
+            chrome.action.setBadgeText({
+                text: count.toString(),
                 tabId
-            })
-        })
+            });
+        });
     }
 }
 
